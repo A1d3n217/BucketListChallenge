0e3339da5e30e50fad2eb74d8c2b2f8ff3fc692d
A1d3n217 <aidenwwood@gmail.com>
2024-10-10 09:56:51 -0600

    Initial Commit

---
+++ BucketListChallenge/BucketListChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListChallengeApp.swift
+//  BucketListChallenge
+//
+//  Created by Wood, Aiden - Student on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
